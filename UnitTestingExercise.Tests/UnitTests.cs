708275043c015d2633dfeed4f9a024132b9020ab
Abdoulahi Cisse <sentel.nv@gmail.com>
2020-11-22 12:12:41 -0800

    exercise unit testing part 1 solved

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,103 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(2, 3, 5, 10)]
+        [InlineData(-4, 7, 15, 18)]
+        [InlineData(0, -3, 8, 5)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Add(num1, num2, num3);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, -5, 7)]
+        [InlineData(0, 5, -5)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
-
+            var actual = test.Substract(minuend, subtrhend);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(3, 0, 0)]
+        [InlineData(2, -5, -10)]
+        [InlineData(-3, -5, 15)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
+
 
             //Act
+            var actual = test.Multiply(num1, num2);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(3, 3, 1)]
+        [InlineData(10, -5, -2)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Divide(num1, num2);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SumOfarr()
+        {
+            //Arrange
+            var test = new UnitTestMethods();
+            int[] r = new int[6] { 3, 2, 4, 5, 3, 1 };
+
+            //Act
+            var actual = test.SumOfArr(r);
+            var expected = 18;
+
+            Assert.Equal(expected, actual);
 
             //Assert
 
         }
 
         [Fact]
-        public void YourMethodName()
+        
+        public void ProductOfOddnums()
         {
+            int[] b = new int[4] { 2, 3, 5, 4 };
             //Arrange
+            var test = new UnitTestMethods();
+            var actual = test.MultiplyOddNums(b);
 
             //Act
+            var expected = 15;
+            Assert.Equal(expected, actual);
+
 
             //Assert
-
-        }
-
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
+           
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -9,18 +9,56 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
-
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
 
         // Create a Multiply method that passes 2 integers
 
+        public int Multiply(int num1, int num2)
+        {
+            return  num1* num2;
+        }
+
         // Create a Divide method that passes 2 integers
+
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
 
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public int SumOfArr(int[] arr)
+        {
+            int sum = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
 
+        public int MultiplyOddNums(int[] arr)
+        {
+            int multOdd = 1;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] % 2 != 0)
+                {
+                    multOdd *= arr[i];
+                }
+            }
+            return multOdd;
+        }
         
     }
 }
